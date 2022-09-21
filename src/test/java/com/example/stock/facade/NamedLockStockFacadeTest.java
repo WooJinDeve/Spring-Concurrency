b4d4c8b7b7abc141ab330394dff1f191cf29926f
WooJinDeve <jwj060111@gmail.com>
2022-09-21 14:18:27 +0900

    [4] DB 동시성 제어 방법

--- src/test/java/com/example/stock/facade/NamedLockStockFacadeTest.java
+++ src/test/java/com/example/stock/facade/NamedLockStockFacadeTest.java
@@ -1,7 +1,9 @@
-package com.example.stock.service;
+package com.example.stock.facade;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import com.example.stock.service.PessimisticLockStockService;
+import com.example.stock.service.StockService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,13 +17,10 @@ import java.util.concurrent.Executors;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class StockServiceTest {
+class NamedLockStockFacadeTest {
 
     @Autowired
-    private StockService stockService;
-
-    @Autowired
-    private PessimisticLockStockService pessimisticLockStockService;
+    private NamedLockStockFacade namedLockStockFacade;
 
     @Autowired
     private StockRepository stockRepository;
@@ -37,16 +36,6 @@ class StockServiceTest {
         stockRepository.deleteAll();
     }
 
-    @Test
-    public void stock_decrease(){
-        stockService.decrease(1L, 1L);
-
-        //100 - 1 = 99
-
-        Stock stock = stockRepository.findById(1L).orElseThrow();
-
-        assertEquals(99, stock.getQuantity());
-    }
 
     @Test
     public void 동시에_100개의_요청() throws InterruptedException {
@@ -59,7 +48,7 @@ class StockServiceTest {
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
-                    pessimisticLockStockService.decrease(1L, 1L);
+                    namedLockStockFacade.decrease(1L, 1L);
                 }finally {
                     latch.countDown();
                 }
@@ -73,6 +62,5 @@ class StockServiceTest {
         // 예상 동작 결과 stockQuantity = 0
         assertEquals(0L, stock.getQuantity());
     }
-
 
 }
--- src/test/java/com/example/stock/facade/OptimisticLockFacadeTest.java
+++ src/test/java/com/example/stock/facade/OptimisticLockFacadeTest.java
@@ -1,12 +1,15 @@
-package com.example.stock.service;
+package com.example.stock.facade;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import com.example.stock.service.PessimisticLockStockService;
+import com.example.stock.service.StockService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -15,13 +18,10 @@ import java.util.concurrent.Executors;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class StockServiceTest {
+class OptimisticLockFacadeTest {
 
     @Autowired
-    private StockService stockService;
-
-    @Autowired
-    private PessimisticLockStockService pessimisticLockStockService;
+    private OptimisticLockFacade optimisticLockFacade;
 
     @Autowired
     private StockRepository stockRepository;
@@ -38,17 +38,6 @@ class StockServiceTest {
     }
 
     @Test
-    public void stock_decrease(){
-        stockService.decrease(1L, 1L);
-
-        //100 - 1 = 99
-
-        Stock stock = stockRepository.findById(1L).orElseThrow();
-
-        assertEquals(99, stock.getQuantity());
-    }
-
-    @Test
     public void 동시에_100개의_요청() throws InterruptedException {
         int threadCount = 100;
         //비동기 요청을 단순하게 실행시켜주는 자바 API
@@ -59,8 +48,10 @@ class StockServiceTest {
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
-                    pessimisticLockStockService.decrease(1L, 1L);
-                }finally {
+                    optimisticLockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
                     latch.countDown();
                 }
             });
@@ -73,6 +64,5 @@ class StockServiceTest {
         // 예상 동작 결과 stockQuantity = 0
         assertEquals(0L, stock.getQuantity());
     }
-
 
 }
